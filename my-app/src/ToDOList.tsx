c839f6c03a7b5a6bacdcf9bd4cc5a3e48b48289c
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2024-01-06 16:08:17 +0100

    fead: add functionality for delete task

--- my-app/src/ToDOList.tsx
+++ my-app/src/ToDOList.tsx
@@ -3,6 +3,11 @@ import tasks from "./constants";
 
 function ToDoList() {
     const [tasksList, setTasksList] = useState(tasks);
+    function gun(id: number) {
+      const filteredTasks = tasksList.filter(item => item.id !== id)
+      setTasksList(filteredTasks)
+      console.log(filteredTasks)
+    }
     return (
       <div>
         <h3>What to do</h3>
@@ -11,7 +16,11 @@ function ToDoList() {
           <button>+</button>
         </div>
         <ul>
-          {tasksList.map((item) => <li><input type='checkbox' checked={item.isDone}/><span>{item.task}</span></li>)}
+          {tasksList.map((item) => <li><input type='checkbox' checked={item.isDone}/>
+              <span>{item.task}</span>
+              <button onClick={() => gun(item.id)}>x</button>
+            </li>
+          )}
         </ul>
         <div>
             <button>All</button>
